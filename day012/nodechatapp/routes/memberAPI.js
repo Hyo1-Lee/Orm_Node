92d36fc6eef366b6e94d1029ecebd173da9a45f5
Hyo1-Lee <gydnjsdl426@naver.com>
2023-12-13 15:56:19 +0900

    member API create edited

--- day012/nodechatapp/routes/memberAPI.js
+++ day012/nodechatapp/routes/memberAPI.js
@@ -3,11 +3,11 @@ const fs = require('fs').promises;
 const path = require('path');
 const router = express.Router();
 
-const MEMBERS_FILE = path.join(__dirname, '../DB/members.json'); // Correct path
+const MEMBERS_FILE = path.join(__dirname, '../DB/members.json');
 
 async function getMembersData() {
-  const data = await fs.readFile(MEMBERS_FILE, 'utf8');
-  return JSON.parse(data);
+    const data = await fs.readFile(MEMBERS_FILE, 'utf8');
+    return JSON.parse(data);
 }
 
 router.get('/all', async (req, res) => {
@@ -19,7 +19,7 @@ router.get('/all', async (req, res) => {
     }
 });
 
-router.post('/create', function(req, res) {
+router.post('/create', async(req, res)=>{
     var member_id = req.body.member_id;
     var email = req.body.email
     var name = req.body.name
@@ -44,14 +44,21 @@ router.post('/create', function(req, res) {
         edit_date
     };
 
-    res.json(member);
+    try {
+        const members = await getMembersData();
+        members.push(member);
+        await fs.writeFile(MEMBERS_FILE, JSON.stringify(members, null, 2), 'utf8');
+        res.json({ message: "Member created successfully", member: member });
+    } catch (error) {
+        res.status(500).json({ message: "Error saving the member", error: error });
+    }
 });
 
-router.post('/modify', function(req, res) {
+router.post('/modify', async(req, res)=>{
     res.json({ message: "Member modified" });
 });
 
-router.post('/delete', function(req, res) {
+router.post('/delete', async(req, res)=>{
     res.json({ message: "Member deleted" });
 });
 
